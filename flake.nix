15122b894a18a946f2f6a4d517c03bda16c82ef7
Richard Halford <dev@rshalford.com>
2023-04-23 12:26:41 +0100

    answers: completed exercises up to 054_manypointers

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "ziglings";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+        with pkgs; {
+          devShells.default = mkShell {
+            buildInputs = [
+              zls
+            ];
+            shellHook = ''
+              export PATH=$PATH:$PWD/zig-0.11.0
+            '';
+          };
+        }
+    );
+}
